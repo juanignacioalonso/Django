c304c12bf091409568c67fb8de9fd3998b808dc6
juanignacioalonso <zalbak87@gmail.com>
2025-09-16 16:10:50 -0300

    Inicio del proyecto

--- Contenedor/Taller/settings.py
+++ Contenedor/Taller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^1wg(a7*qp$k-+w*h3b^3=-9h6c#lrlx8wnyq(5+@ezg_hl6-'
+SECRET_KEY = '1wg(a7*qp$k-+w*h3b^3=-9h6c#lrlx8wnyq(5+@ezg_hl6-8798'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
